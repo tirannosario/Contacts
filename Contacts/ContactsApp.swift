b889538e93e1ceedb1bb48c752f6d64696cab8fc
tirannosario <glt.rosario@gmail.com>
2022-07-01 10:40:52 +0200

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Rosario Galioto on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
